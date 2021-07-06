ad9aa739ebab8d4dff6d4553612b78872501861c
Roland Nemes <nemesroland2@gmail.com>
2021-07-06 20:00:10 +0300

    Autocommit.

--- config.def.h
+++ config.def.h
@@ -1,27 +1,16 @@
-/* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
-
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int topbar = 1;                    /* -b  option; if 0, dmenu appears at bottom */
+static int fuzzy = 1;                     /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 static const char *fonts[] = {
-	"DejaVu Sans Mono:size=9"
+	"DejaVu Sans Mono:size=10"            /* -fn option overrides fonts[0]; default X11 font or font set */
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;         /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+                 /*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
-/* -h option; minimum height of a menu line */
-static unsigned int lineheight = 20;
-static unsigned int min_lineheight = 8;
-
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
-static const char worddelimiters[] = " ";
+static unsigned int lines      = 0;       /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lineheight = 20;      /* height of the menu line, usually set to the same size as the bar behind it */
+static unsigned int min_lineheight = 8;   /* -h option; minimum height of a menu line */
+static const char worddelimiters[] = " "; /* Characters not considered part of a word while deleting words, for example: " /?\"&[]" */
