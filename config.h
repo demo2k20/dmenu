fa3027875b75281f6234d6344df5ea6ef771464a
Roland Nemes <nemesroland2@gmail.com>
2021-08-18 02:38:23 +0300

    Autocommit.

--- config.h
+++ config.h
@@ -5,10 +5,11 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                       /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "Terminus:size=8";
+static char font[] = "Terminus:size=10";
 static const char *fonts[] = {
 	font,
-	"Terminus:size=8",
+	"Terminus:size=10",
+	"JoyPixels:size=10",
 };
 
 static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
